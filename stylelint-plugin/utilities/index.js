1cfe3cc85bb810e1783a03df0df7450b6bdb89d0
portikM <maksym.portianoi@konghq.com>
2023-07-05 17:21:06 -0400

    chore: rebase

--- stylelint-plugin/utilities/index.js
+++ stylelint-plugin/utilities/index.js
@@ -1,8 +1,9 @@
-const { KONG_TOKEN_PREFIX, PROPERTY_TOKEN_MAP } = require('./constants')
+const { KONG_TOKEN_PREFIX, PROPERTY_TOKEN_MAP, RULE_NAME_PREFIX } = require('./constants')
 const extractTokensFromValue = require('./extract-tokens-from-value')
 
 module.exports = {
   KONG_TOKEN_PREFIX,
   PROPERTY_TOKEN_MAP,
   extractTokensFromValue,
+  RULE_NAME_PREFIX,
 }
