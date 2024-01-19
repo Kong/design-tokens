57c29d30583728bac22b92c8a1a750a35fc634b6
portikM <maksym.portianoi@konghq.com>
2024-01-19 16:12:19 -0500

    chore(stylelint-plugin): setup vitest [KHCP-10186]

---
+++ stylelint-plugin/rules/use-proper-token/index.test.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest'
+
+describe('use-proper-token', () => {
+  it('dummy passing test', () => {
+    expect(true).toBe(true)
+  })
+
+  // it('dummy failing test', () => {
+  //   expect(true).toBe(false)
+  // })
+})
